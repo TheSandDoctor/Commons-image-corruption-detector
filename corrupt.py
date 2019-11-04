8f1a953dbf1f4afa316dabe384ea96407261c9a6
TheSandDoctor <twitter@markyrosongaming.com>
2019-11-04 00:05:52 -0800

    Some cleanup

--- corrupt.py
+++ corrupt.py
@@ -1,10 +1,3 @@
-# from PIL import Image
-# try:
-#	img = Image.open("./Test.jpg")
-#	img.verify()
-#	print("verified")
-# except (IOError, SyntaxError) as e:
-#	print("Bad file")
 from __future__ import absolute_import
 
 import traceback
@@ -17,14 +10,13 @@ ImageFile.MAXBLOCK = 1
 
 
 def detect(f):
-  #  with FileProxy(open(f, 'rb')) as f:
     try:
         image = Image.open(f)
 
         image.tobytes()
         print("Works")
     except Exception as e:
-        print("Corrupt\n\n")
+        print("Corrupt\n\n") # If we get this far, image is corrupt
         print(e)
 
 
