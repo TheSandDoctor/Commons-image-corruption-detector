012a346e3d92fa2114c9ff40cbb446170b8cfbcf
TheSandDoctor <majorjohn1@mail.com>
2019-11-21 10:03:14 -0800

    Removal of more mwclient implementations
    Removed mwclient implementations from corrupt.py and image_corruption_utils.py. Updated call in
    image_followup.py.
    
    Closes T27
    Closes T28

--- corrupt.py
+++ corrupt.py
@@ -1,4 +1,4 @@
-#TODO: Should the corruption checking methods etc be forked into their own
+# TODO: Should the corruption checking methods etc be forked into their own
 # file (for reuse in others)?
 
 from __future__ import absolute_import
@@ -13,55 +13,31 @@ import pywikibot
 import pwb_wrappers
 import os
 
-
 number_saved = 0
 
-def save_page_PWB(site, page, text, edit_summary, isBotEdit = True, isMinor = True):
+
+# Save edit, we aren't checking if we are exclusion compliant as that isn't relevant in this task
+def save_page(site, page, text, edit_summary, is_bot_edit=True, is_minor=True):
     if not call_home(site, "full_scan"):
         raise ValueError("Kill switch on-wiki is false. Terminating program.")
     retry_apierror(
         lambda:
-        filepage.save(appendtext=msg, section=new, #FIXME: appendtext and section=new surely don't play together(?)
-                      summary=summary, minor=True, botflag=True, force=True)
+        page.save(appendtext=text, section='new',  # FIXME: appendtext and section=new surely don't play together(?)
+                  summary=edit_summary, minor=is_minor, botflag=is_bot_edit, force=True)
     )
 
-# Save edit, we aren't checking if we are exclusion compliant as that isn't relevant in this task
-#TODO: Convert this to pwb - this will probably simplify the code somewhat
-def save_page(site, page, text, edit_summary, isBotEdit = True, isMinor = True):
-    if not call_home(site, "full_scan"):
-        raise ValueError("Kill switch on-wiki is false. Terminating program.")
-    time = 0
-    while True:
-        if time == 1:
-            text = site.Pages["File:" + page.page_title].text()
-        try:
-            page.save(text,summary=edit_summary, bot=isBotEdit, minor=isMinor)
-            print("Saved page")
-            global number_saved
-            number_saved += 1
-            if time == 1:
-                time = 0
-            break
-        except [[EditError]]:
-            print("Error")
-            time = 1
-            sleep(5) # sleep for 5 seconds before trying again
-            continue
-        except [[ProtectedPageError]]:
-            print('Could not edit ' + page.page_title + ' due to protection')
-        break
 
-def process_file_PWB(image_page, site):
-    text = failed = hash = None
-    _, ext = os.path.splitext(image_page.title())   #TODO: reduce this to not include "File:"?
+def process_file(image_page, site):
+    text = failed = img_hash = None
+    _, ext = os.path.splitext(image_page.title())  # TODO: reduce this to not include "File:"?
     download_attempts = 0
     while True:
         with open('./Example' + ext, 'wb') as fd:
             image_page.download(fd)
 
-        hashResult, hash = verifyHash(site, "./Example" + ext, image_page)
-        if not hashResult:
-            if download_attempts => 10:
+        hash_result, img_hash = verifyHash(site, "./Example" + ext, image_page)
+        if not hash_result:
+            if download_attempts >= 10:
                 failed = 1
                 break
             download_attempts += 1
@@ -69,74 +45,35 @@ def process_file_PWB(image_page, site):
         else:
             break
     if failed:
-        raise ValueError("Hash check failed for " + "./Example" + ext + " vs " + str(image_page.title()) + " " + download_attempts + " times. Aborting...")
+        raise ValueError("Hash check failed for " + "./Example" + ext + " vs " + str(
+            image_page.title()) + " " + download_attempts + " times. Aborting...")
     del download_attempts
     # Read and check if valid
     with open("./Example" + ext, "rb") as f:
         try:
             result = image_is_corrupt(f)
         except FileFormatError:
-            os.remove('./Example' + ext)    # file not an image
+            os.remove('./Example' + ext)  # file not an image
             raise
-    del ext # no longer a needed variable
-    if result: # image corrupt
-        text = pwb_wrappers.tag_page(image_page, "{{Template:User:TheSandDoctor/Template:TSB image identified corrupt|" + datetime.now(timezone.utc).strftime("%Y-%m-%d") + "}}",
-            "Image detected as corrupt, tagging.")
-        store_image(image_page.title(), True, hash = hash) # store in database
+    del ext  # no longer a needed variable
+    if result:  # image corrupt
+        text = pwb_wrappers.tag_page(image_page,
+                                     "{{Template:User:TheSandDoctor/Template:TSB image identified corrupt|" +
+                                     datetime.now(
+                                         timezone.utc).strftime("%Y-%m-%d") + "}}",
+                                     "Image detected as corrupt, tagging.")
+        store_image(image_page.title(), True, hash=img_hash)  # store in database
         print("Saved page and logged in database")
         # Notify the user that the file needs updating
-        try: #TODO: Add record to database about successful notification?
-            notifyUser_PWB(site, image_page, "30 days", "full_scan")
-        except: #TODO: Add record to database about failed notification?
+        try:  # TODO: Add record to database about successful notification?
+            notifyUser(site, image_page, "30 days", "full_scan")
+        except:  # TODO: Add record to database about failed notification?
             print("ERROR: Could not notify user about " + str(image_page.title()) + " being corrupt.")
-    else: # image not corrupt
-        store_image(image_page.title(), False, hash = hash) # store in database
+    else:  # image not corrupt
+        store_image(image_page.title(), False, hash=img_hash)  # store in database
 
 
-# Process image
-def process_file(image_page, site):
-    text = failed = hash = None
-    _, ext = os.path.splitext(image_page.page_title)    # get filetype
-    download_attempts = 0
-    # Download image
-    while True:
-        with open("./Example" + ext,"wb") as fd:
-            image_page.download(fd)
-        hashResult, hash = verifyHash(site, "./Example" + ext, image_page)
-        if not hashResult:
-            if download_attempts => 10:
-                failed = 1
-                break
-            download_attempts += 1
-            continue
-        else:
-            break
-    if failed:
-        raise ValueError("Hash check failed for " + "./Example" + ext + " vs " + str(image_page.name) + " " + download_attempts + " times. Aborting...")
-    del download_attempts
-    # Read and check if valid
-    with open("./Example" + ext, "rb") as f:
-        try:
-            result = image_is_corrupt(f)
-        except FileFormatError:
-            os.remove("./Example" + ext)    # file not an image.
-            raise
-    del ext # no longer a needed variable
-    if result: # image corrupt
-        text = tag_page(image_page, site, "{{Template:User:TheSandDoctor/Template:TSB image identified corrupt|" + datetime.now(timezone.utc).strftime("%Y-%m-%d") + "}}")
-        save_page(site, image_page, text,"Image detected as corrupt, tagging.")
-        store_image(image_page.name, True, hash = hash) # store in database
-        print("Saved page and logged in database")
-        # Notify the user that the file needs updating
-        try: #TODO: Add record to database about successful notification?
-            notifyUser(site, image_page, getUploaderAndTimestamp(site, str(image_page.name)), "30 days", "full_scan")
-        except: #TODO: Add record to database about failed notification?
-            print("ERROR: Could not notify user about " + str(image_page.name) + " being corrupt.")
-    else: # image not corrupt
-        store_image(image_page.name, False, hash = hash) # store in database
-
-
-def run_PWB(utils):
+def run(utils):
     site = utils[1]
     offset = utils[2]
     for page in pwb_wrappers.allimages():
@@ -145,7 +82,7 @@ def run_PWB(utils):
             print("Skipped due to offset config")
             continue
         print("Working with: " + str(page.title()))
-        #print(number_saved)
+        # print(number_saved)
         text = page.text
         try:
             if have_seen_image(site, page.title()):
@@ -153,74 +90,29 @@ def run_PWB(utils):
                 continue
             try:
                 process_file(page, site)
-            except FileFormatError as e: # File not an image. Best to just continue
+            except FileFormatError as e:  # File not an image. Best to just continue
                 continue
             except ValueError as e2:
                 print(e2)
                 with open('downloads_failed.txt', 'a+') as f:
-                    print(e2,file=f) # print to file
+                    print(e2, file=f)  # print to file
                 continue
         except ValueError:
             raise
     return
 
 
-def run(utils):
-    site = utils[1]
-    offset = utils[2]
-    limit = utils[3]
-    global number_saved
-    for page in site.allimages(): # allimages() avoids issues with non-images
-        if offset > 0:
-            offset -= 1
-            print("Skipped due to offset config")
-            continue
-        print("Working with: " + page.name)
-        print(number_saved)
-        if number_saved < limit:
-            text = page.text()
-            try:
-                if have_seen_image(site, page.name):
-                    print("Found duplicate, no need to check")
-                    continue
-                try:
-                    process_file(page, site)
-                except FileFormatError as e: # File not an image. Best to just continue
-                    continue
-                except ValueError as e2:
-                    print(e2)
-                    with open('downloads_failed.txt', 'a+') as f:
-                        print(e2,file=f) # print to file
-                    continue
-            except ValueError:
-                raise
-        else:
-            return # tun out of pages in limited run
-
-
-def main_PWB():
-     site = pywikibot.Site(code='commons', fam='commons', user='TheSandBot')
-     lresult = site.login()
-     if not lresult:
-         raise ValueError('Incorrect password')
-    offset = 0
-    utils = [config,site,offset]
-    run(utils)
-
 def main():
-    site = mwclient.Site(('https', 'commons.wikimedia.org'), '/w/')
-    config = configparser.RawConfigParser()
-    config.read('credentials.txt')
-    try:
-        site.login(config.get('enwiki_sandbot', 'username'), config.get('enwiki_sandbot', 'password'))
-    except errors.LoginError as e:
-        print(e)
-        raise ValueError("Login failed")
+    config = None
+    site = pywikibot.Site(code='commons', fam='commons', user='TheSandBot')
+    lresult = site.login()
+    if not lresult:
+        raise ValueError('Incorrect password')
     offset = 0
-    limit = 2
-    utils = [config,site,offset,limit]
+    utils = [config, site, offset]
     run(utils)
+
 
 if __name__ == '__main__':
-    #main()
+    # main()
     pass
--- image_corruption_utils.py
+++ image_corruption_utils.py
@@ -54,7 +54,7 @@ def getLocalHash(filename):
     # return the hex representation of digest
     return h.hexdigest()
 
-def getRemoteHash_PWB(site, filename):
+def getRemoteHash(site, filename):
     """
     Get remote hash from Wikimedia Commons.
 
@@ -67,27 +67,6 @@ def getRemoteHash_PWB(site, filename):
     """
     fp = pywikibot.FilePage(site, filename)
     return str(fp.latest_file_info.sha1)
-
-def getRemoteHash(site, filename):
-    """
-    Fetch remote hash from the mediawiki API.
-
-    An example of this in use:
-    https://commons.wikimedia.org/wiki/Special:ApiSandbox#action=query&format=json&prop=imageinfo&titles=File%3ASalda%C3%B1a%20-%20015%20(26238038617).jpg&iiprop=timestamp%7Cuser%7Csha1
-    :param site: site object
-    :param filename: file name to get the remote hash from (string)
-    :return: sha1 hash in the form of a string.
-    """
-    #
-    result = site.api('query', prop = 'imageinfo', iiprop = 'timestamp|user|sha1', titles=filename)
-    pageid = sha = None
-    for i in result['query']['pages']:
-        pageid = str(i)
-    for i in result['query']['pages'][pageid]['imageinfo']:
-        sha = i['sha1']
-    del pageid
-    return sha
-
 
 def verifyHash(site, local, image_page):
     """
@@ -102,40 +81,18 @@ def verifyHash(site, local, image_page):
     result = lhash == rhash
     return [result, rhash]
 
-def getUploaderAndTimestamp_PWB(site, filename):
+def getUploaderAndTimestamp(site, filename):
     # https://github.com/wikimedia/pywikibot/blob/298ff28eacb0cd50cca8ad19484758daab05d86c/pywikibot/page.py#L2634
     fp = pywikibot.FilePage(site, filename)
     return [str(fp.latest_file_info.user),
                 UnicodeType(fp.latest_file_info.timestamp.isoformat())]
 
-#TODO: verify functionality
-def getUploaderAndTimestamp(site, filename):
-    """
-    Get most recent file uploader and timestamp of that upload. This method
-    may potentially throw an error due to issues with the mediawiki software itself
-    it is unclear and unforeseeable what the specific errors may be at this time,
-    it is just a distinct possibility. As such, this method should _always_ be wrapped in
-    a try/except statement.
-    :param site: site object
-    :param filename: filename to fetch the information of (string)
-    :return: [user, timestamp] list (user and timestamp both strings)
-    """
-    result = site.api('query', prop = 'imageinfo', iiprop = 'timestamp|user|sha1', titles=filename)
-    pageid = user = timestamp = None
-    for i in result['query']['pages']:
-        pageid = str(i)
-    for i in result['query']['pages'][pageid]['imageinfo']:
-        user = i['user']
-        timestamp = i['timestamp']
-    del pageid
-    return [user, timestamp]
 
-
-def notifyUser_PWB(site, image, time_duration, task_name, minor = True, day_count = None):
+def notifyUser(site, image, time_duration, task_name, minor = True, day_count = None):
     if not call_home(site, task_name):
         raise ValueError("Kill switch on-wiki is false. Terminating program.")
         
-    user, timestamp = getUploaderAndTimestamp_PWB(site, image)
+    user, timestamp = getUploaderAndTimestamp(site, image)
     tp = pywikibot.Page(site, "User talk:" + user)
     if task_name == 'full_scan' or task_name == 'monitor':
         msg = "{{subst:TSB corruption notification|user=" + str(user) + "|file=" + str(image.title()) + "|time=" + str(timestamp)
@@ -156,54 +113,12 @@ def notifyUser_PWB(site, image, time_duration, task_name, minor = True, day_coun
         #summary = "Notify about corrupt image [[" + str(image.title()) + "]]" + " nomination for [[Commons:CSD|speedy deletion]]"
         print("Notification of CSD nomination of " + str(image.title()))
 
-    retry_apierror(
-        lambda:
-        tp.save(appendtext=msg, section='new', #FIXME: appendtext and section=new surely don't play together(?)
-                      summary=summary, minor=minor, botflag=True, force=True)
-    )
-
-#TODO: Formalize/improve further
-def notifyUser(site, image, user, time_duration, task_name):
-    if not call_home(site, task_name):
-        raise ValueError("Kill switch on-wiki is false. Terminating program.")
-    #time = 0
-    msg = "Hello " + user + ", it appears that the version of [[" + str(image.name) + "]] which you uploaded " + user[1]
-    msg += " is broken or corrupt. Please review the image and attempt to correct this issue by uploading a new version of the file. [[User:TheSandBot|TheSandBot]] will re-review this image again in " + time_duration
-    msg += " if it is not resolved by then, the file will be [[Commons:CSD|nominated for deletion]] automatically."
-    user_talk = site.Pages['User talk:' + user[0]]
-    while True:
-        try:
-            user_talk.append(msg,summary="Notify about corrupt image [[" + str(image.name) + "]]", bot=True, minor=False, section='new')
-            print("Notified user of corrupt " + str(image.name))
-        #    if time == 1:
-            #    time = 0
-            break
-        except [[EditError]]:
-            print("Error")
-            #time = 1
-            sleep(5) # sleep for 5 seconds before trying again
-            continue
-        except [[ProtectedPageError]]:
-            print('Could not edit [[User talk:' + user[0] + ']] due to protection')
-            break
+        # FIXME: appendtext and section=new surely don't play together(?)
+        retry_apierror(lambda:tp.save(appendtext=msg, section='new',summary=summary, minor=minor, botflag=True, force=True))
 
 
-# Add template to image page
-def tag_page(page, site, tag):
-    text = page.text()
-    text = tag + "\n" + text
-    return text
-
-def call_home_PWB(site_obj, key):
+def call_home(site_obj, key):
     page = pywikibot.Page(site_obj, 'User:TheSandBot/status')
     text = page.text
     data = json.loads(text)["run"]["corrupt_image_finder"][key]
     return str(data) == str(True)
-
-def call_home(site_obj, key):
-    page = site_obj.Pages['User:TheSandBot/status']
-    text = page.text()
-    data = json.loads(text)["run"]["corrupt_image_finder"][key]
-    if str(data) == str(True):
-        return True
-    return False
