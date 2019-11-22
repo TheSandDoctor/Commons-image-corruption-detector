b692c4258bc664e3c1ed001646201eba2acea7ca
TheSandDoctor <majorjohn1@mail.com>
2019-11-22 07:38:07 -0800

    Improve documentation
    Improved documentation in database_stuff.py and improve PEP8 compliance (T7).

--- database_stuff.py
+++ database_stuff.py
@@ -4,37 +4,55 @@ import mysql.connector
 from image_corruption_utils import getRemoteHash
 
 config = {
-  'user': 'scott',
-  'password': 'password',
-  'host': '127.0.0.1',
-  'database': 'images',
-  'raise_on_warnings': True
+    'user': 'scott',
+    'password': 'password',
+    'host': '127.0.0.1',
+    'database': 'images',
+    'raise_on_warnings': True
 }
 insert_image = ("INSERT INTO images_viewed "
                 "(title, isCorrupt, date_scanned, to_delete_nom, hash) "
                 "VALUES (%(title)s, %(isCorrupt)s, %(date_scanned)s, %(to_delete_nom)s, %(hash)s)")
 
 expired_images = {"SELECT title, isCorrupt, date_scanned, to_delete_nom FROM images_viewed"
-                "WHERE to_delete_nom = %s"}
+                  "WHERE to_delete_nom = %s"}
 
 update_entry = {"UPDATE images_viewed SET isCorrupt = %s, to_delete_nom = %s, hash = %s WHERE title = %s"}
 
+
 def getNextMonth(day_count):
+    """
+    Generate date timestamp from now until the provided day_count.
+    :param day_count: how many days from now to calculate the date of (int)
+    :return: Date string in format m/d/yyyy
+    """
     return (datetime.now(timezone.utc).date() + timedelta(days=day_count)).strftime('%m/%d/%Y')
 
 
 def calculateDifference(date_tagged):
-    """ Returns the difference in days (int) between current date and provided date_tagged
-        date_tagged: Date string """
+    """
+    Calculate the difference between the current date and a provided date string
+    :param date_tagged: Date string
+    :return: the difference in days (int) between current date and provided date_tagged
+    """
     date_tagged = datetime.strptime(date_tagged, '%m/%d/%Y').date()
     return (datetime.now(timezone.utc).date() - date_tagged).days
 
 
 def store_image(title, isCorrupt, img_hash, day_count=30):
+    """
+    Stores current image information (provided in header) into the database for this application.
+    :param title: filename
+    :param isCorrupt: (bool)
+    :param img_hash: sha1 hash of image
+    :param day_count: how long a grace period pre-nomination if unresolved by then (default: 30 days, not stored
+    if image isn't corrupt in the first place (database defaults to NULL if no value provided)
+    :return: None
+    """
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor()
     if isCorrupt:
-        image_info = {
+        image_data = {
             'title': title,
             'isCorrupt': isCorrupt,
             'date_scanned': datetime.now(timezone.utc).date().strftime('%m/%d/%Y'),
@@ -55,26 +73,35 @@ def store_image(title, isCorrupt, img_hash, day_count=30):
 
 
 def get_expired_images():
-    """ Returns images whose expiry date is today (UTC) as tuples.
-        Return data structure is as follows:
-            0th element: title
-            1st element: isCorrupt
-            2nd element: date_scanned
-            3rd element: to_delete_nom
+    """
+    Get images whose expiry date is today (UTC)
+    Return data structure is as follows:
+           0th element: title
+           1st element: isCorrupt
+           2nd element: date_scanned
+           3rd element: to_delete_nom
+    :return: images whose expiry date is today (UTC) as tuples.
     """
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor()
     cursor.execute(expired_images, datetime.now(timezone.utc).date().strftime('%m/%d/%Y'))
-    raw = cursor.fetchall() # returns tuples
+    raw = cursor.fetchall()  # returns tuples
     cnx.close()
     return raw
-    #data = []
-    #for i in raw:
+    # data = []
+    # for i in raw:
     #    data.append(i[0])
-    #return data
+    # return data
 
 
 def have_seen_image(site, title):
+    """
+    Checks if we have previously viewed this image with its current hash value. This is done through connecting to the
+    database for this application.
+    :param site: site object
+    :param title: filename to check
+    :return: True if seen, False if not
+    """
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor()
     img_hash = getRemoteHash(site, title)
@@ -87,7 +114,16 @@ def have_seen_image(site, title):
     #    return False
     # return True
 
+
 def update_entry(title, isCorrupt, to_delete_nom, img_hash):
+    """
+    Updates existing entry in database. This is currently called in image_followup when an image has been corrected.
+    :param title: filename
+    :param isCorrupt: (bool)
+    :param to_delete_nom: date string for when to nominate for deletion (NULL if not corrupt)
+    :param img_hash: hash of the image to compare with the stored database value
+    :return: None
+    """
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor()
     cursor.execute(update_entry, (isCorrupt, to_delete_nom, img_hash, title))
