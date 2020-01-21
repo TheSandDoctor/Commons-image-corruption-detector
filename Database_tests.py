a9e882cb9c5a4b505ff7fe8817486e08531bb7ee
TheSandDoctor <majorjohn1@mail.com>
2020-01-20 21:23:48 -0800

    Specify new location of test files

--- Database_tests.py
+++ Database_tests.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     # Currently testing the ability to add hashes and connect. The isCorrupt field is deliberately set as well and is
     # not authentic to the image at hand (for this test it doesn't matter).
     ###
-    database_stuff.store_image("test1.jpg", False, image_corruption_utils.getLocalHash("test1.jpg"), 7)
-    database_stuff.store_image("test1.jpg", True, image_corruption_utils.getLocalHash("test2.jpg"))
+    database_stuff.store_image("tests/test1.jpg", False, image_corruption_utils.getLocalHash("tests/test1.jpg"), 7)
+    database_stuff.store_image("tests/test1.jpg", True, image_corruption_utils.getLocalHash("tests/test2.jpg"))
     #database_stuff.update_entry("test1.jpg", True, datetime.now(timezone.utc).date().strftime('%B/%d/%Y'), image_corruption_utils.getLocalHash("test.jpg"))
