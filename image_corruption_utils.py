986b373bc61a5e17089e224ae10c3e4c9334091f
TheSandDoctor <twitter@markyrosongaming.com>
2020-01-25 20:04:57 -0800

    Resolve getLocalHash() timeout
    chunk was written as chuck in some places, which resulted in an infinite loop. This has been resolved and the code no longer times out.
    
    Closes T62

--- image_corruption_utils.py
+++ image_corruption_utils.py
@@ -51,8 +51,8 @@ def getLocalHash(filename):
         chunk = 0
         while chunk != b'':
             # read only 1024 bytes at a time
-            chuck = file.read(1024)
-            h.update(chuck)
+            chunk = file.read(1024)
+            h.update(chunk)
 
     return h.hexdigest()
 
