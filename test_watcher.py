d3feae8e006ec2b770fe5bdaed27c4049e24fffc
TheSandDoctor <twitter@markyrosongaming.com>
2020-02-07 17:03:27 -0800

    Repair test

--- test_watcher.py
+++ test_watcher.py
@@ -5,12 +5,16 @@ from Image import ImageObj
 site = pywikibot.Site(user="TheSandBot")
 rc = site_rc_listener(site)
 myl = []
+count = 0
 for change in rc:
 
     if (
             change['type'] == 'log' and
             change['namespace'] == 6 and
-            change['log_type'] == 'upload'
+            change['log_type'] == 'upload' and count <= 5
     ):
         myl.append(ImageObj(change))
+        count += 1
+    elif count > 5:
+        break
 print(myl)
