3274d3737710de790a6c3c1d4f3e2e63ace28f41
TheSandDoctor <twitter@markyrosongaming.com>
2020-02-09 18:38:15 -0800

    Add KeyboardInterrupt catch
    Ref T111

--- corrupt.py
+++ corrupt.py
@@ -214,14 +214,17 @@ def process_file(image_page, site):
 
 
 if __name__ == '__main__':
-    fileConfig('logging_config.ini')
-    logger = logging.getLogger('full_scan')
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--fs', "--full_scan", help="whether to skip rechecks", action="store_true")
-    args = parser.parse_args()
     try:
-        if args.full_scan:
-            skip = False
-    except AttributeError:
+        fileConfig('logging_config.ini')
+        logger = logging.getLogger('full_scan')
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--fs', "--full_scan", help="whether to skip rechecks", action="store_true")
+        args = parser.parse_args()
+        try:
+            if args.full_scan:
+                skip = False
+        except AttributeError:
+            pass
+        process_file2()
+    except KeyboardInterrupt:
         pass
-    process_file2()
