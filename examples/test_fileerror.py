66a4ec81970e0fae7c61f2374ff973459a6f5104
TheSandDoctor <twitter@markyrosongaming.com>
2020-02-12 22:28:07 -0800

    Add another test for Geagea uploads flagged corrupt
    Ref T121

--- examples/test_fileerror.py
+++ examples/test_fileerror.py
@@ -36,4 +36,5 @@ if __name__ == '__main__':
     test("./Schild,_objectnr_KA_15728.tif")
     test('./Shooting_with_Fire.jpg')
     #test("./David_Ben-Gurion_pins_the_Haganah_ribbon_on_Chaim_Laskov._D358-069.jpg")
-    test("./Moshe_Prager,_Jerusalem_1967.jpg")
+    #test("./Moshe_Prager,_Jerusalem_1967.jpg")
+    test("./Haim_Laskov_-_Yackov_Dori_-_David_Ben-Gurion_-_Hagana_Pin_1958.jpg")
